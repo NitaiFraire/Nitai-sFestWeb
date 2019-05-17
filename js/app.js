c46ad6a78d84557586fbf33f11740d3ba558957c
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-05-17 17:07:46 -0500

    funciones smooth scroll y cambio de color

--- js/app.js
+++ js/app.js
@@ -3,3 +3,28 @@ $(document).ready(function(){
         $(this).html(evento.strftime('<span> %D </span> Dias <span> %H </span> Horas <span> %M </span> Minutos <span> %S </span> Segundos'))
     })
 });
+
+// scroll nav
+window.onscroll = function(e){
+    const scroll = window.scrollY;
+    const header = document.querySelector('#navegacion-principal');
+    
+    if(scroll > 300){
+        header.classList.add('bg-success');
+    }else{
+        header.classList.remove('bg-success');
+    }
+}
+
+// smooth scroll
+document.querySelectorAll('.navbar .nav-link').forEach(enlace => {
+    enlace.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        document.querySelector(enlace.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+
+    });
+});
